d7ddf2cf9c4393cce14f744e5cdb973e2c24e889
seoin <befine324@gmail.com>
2021-11-01 12:39:47 +0900

    개발환경구축

---
+++ CH01/개발 환경 구축하기/hello-world/simple1.js
@@ -0,0 +1,27 @@
+function LikeButton() {
+    const [liked, setLiked] = React.useState(false);
+    const text = liked ? '좋아요 취소' : '좋아요';
+    return React.createElement(
+        'button',
+        { onClick: () => setLiked(!liked)},
+        text,
+    );
+}
+
+const domContainer1 = document.getElementById('root');
+ReactDOM.render(
+    React.createElement(
+        'div',
+        null,
+        React.createElement(LikeButton),
+        React.createElement(LikeButton),
+        React.createElement(LikeButton),
+ ), domContainer1);
+
+// const domContainer2 = document.getElementById('root2');
+// ReactDOM.render(React.createElement(LikeButton), domContainer2);
+
+// const domContainer3 = document.getElementById('root3');
+// ReactDOM.render(React.createElement(LikeButton), domContainer3);
+
+
