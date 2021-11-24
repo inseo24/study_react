8f16ff09d5b45aadada31ea09362148d9f1e528a
seoin <befine324@gmail.com>
2021-11-24 16:06:01 +0900

    add: CH06, change directory

---
+++ 실전리액트프로그래밍/CH03/inflearn-react-project/game2048/start/src/component/Game.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { times } from "lodash";
+import { MAX_POS } from "../constant";
+import { getInitalTileList } from "../util/tile";
+import useMoveTile from "../hook/useMoveTile";
+import Tile from "./Tile";
+
+export default function Game({ setScore }) {
+    const [tileList, setTileList] = useState(getInitalTileList);
+
+    // up, down, left, right
+    useMoveTile({ tileList, setTileList, setScore });
+
+    return (
+        <div className="game-container">
+            <div className="grid-container">
+                {times(MAX_POS, (index) => (
+                    <div key={index} className="grid-row">
+                        {times(MAX_POS, (index2) => (
+                            <div key={index2} className="grid-cell"></div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+
+            <div className="tile-container">
+                {tileList.map((item) => (
+                    <Tile key={item.id} {...item} />
+                ))}
+            </div>
+        </div>
+    );
+}
